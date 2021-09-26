a946db6a8617835bfc38536186529c99cf19ef6e
Manupriya <manuupriya@gmail.com>
2021-09-26 15:34:09 -0700

    adding resources

---
+++ Resources/data-12-1-2-resources/plots.js
@@ -0,0 +1,10 @@
+// Plotly.newPlot("plotArea", [{x: [1, 2, 3], y: [10, 20, 30]}]);
+
+
+var words = ['seal', 'dog', 'scorpion', 'orangutan', 'salamander'];
+var doubled = words.filter(funstion(word){
+    return word[0] == s
+};
+    
+console.log(doubled);
+
